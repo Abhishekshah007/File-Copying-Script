589f909bce9a0da9dedc2ed024851ea1b397b10d
Abhishek shah <amitkumarshah499@gmail.com>
2023-01-24 15:03:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const readline = require("readline-sync");
 let main_file = String(readline.question("Enter File Name With Extention TO Copy:"));
 let noOfTimes = Number(readline.question("No. of time to copy ?"))
 
+// removing file extenstion
+let result = main_file.split('.')
+    
+
+
 for (let index = 0; index <=noOfTimes; index++) {
 
-     fs.copyFile(main_file, `${main_file} - copying${(index==0)?'': (index)}.txt`, fs.constants.COPYFILE_FICLONE, err => {
+     fs.copyFile(main_file, `${result[0]} - copying${(index==0)?'': index}.txt`, fs.constants.COPYFILE_FICLONE, err => {
             if (err) {
                 console.error(err)
             }
